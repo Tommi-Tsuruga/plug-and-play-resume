c06359008d78ab3a4fe2e5c8560a39f3c4903173
Tommi-Tsuruga <tommi.tsuruga@gmail.com>
2019-10-29 20:47:09 -0400

    beforeredux

---
+++ plugandplayresume/frontend/src/components/resumes/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import Form from './Form';
+import Section from './Sections';
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form />
+            <Section />
+        </Fragment>
+    )
+}
---
+++ plugandplayresume/frontend/src/components/resumes/Sections.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Sections extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Resume Sections</h1>
+            </div>
+        )
+    }
+}
