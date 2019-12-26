bd342c52b72441de2fce0d1d7d72482a7bfdaa07
keisuke <e40keisuke@gmail.com>
2019-12-26 02:53:59 -0500

    deploy

--- plug-and-play-resume/settings.py
+++ plug-and-play-resume/settings.py
@@ -98,7 +98,7 @@ CSRF_TRUSTED_ORIGINS = ['localhost:3000']
 if 'RDS_HOSTNAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
